78ef532dc3866bf66effc70833c5b14f58a1ccf5
Mark Cipolla <mark@markcipolla.com>
2025-01-17 09:48:14 +1100

    test: add  from Testing Library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event'
+
+const user = userEvent.setup()
 
 test('renders the example data', () => {
   render(<App />);
